64baedccd43661bc1045b7a190099746b8a05011
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-29 01:58:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_value = 0
+  result_name = ""
+  key_for_min_value.collect do |name, value|
+    if value < lowest_value
+      lowest_value = value
+      result_name = name
+    end
+  end
+  result_name
 end
