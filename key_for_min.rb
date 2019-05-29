34bb074730ea827c74aa532cec3975e012f8a9a9
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-29 02:16:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,7 @@ def key_for_min_value(name_hash)
   result_name = ""
   name_hash.each do |name, value|
     if value < lowest_value
-      #lowest_value = value
-      result_name = name
+      lowest_value = value
     end
   end
   result_name
